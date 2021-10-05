70dd22bb211f8950f904705e03d0d410513973ec
Chris Johnson <Johnny.C.Alexander@gmail.com>
2021-10-04 20:37:40 -0500

    added login and nav components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import './App.css';
+import Nav from './components/Nav';
+import Login from './components/Login';
 
 function App() {
-  return null;
+  return (
+    <main>
+      <Nav />
+      <Login />
+    </main>
+  );
 }
 
 export default App;
