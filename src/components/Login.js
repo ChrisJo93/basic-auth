26d16a57187f09ba65e54ffa9dd69134c42a0ff7
Chris Johnson <Johnny.C.Alexander@gmail.com>
2021-10-04 22:39:51 -0500

    login form in progress

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,18 @@
 import React from 'react';
 
 export default function Login() {
-  return null;
+  return (
+    <section className="login_form_container">
+      <form className="login_form">
+        <input
+          type="text"
+          label="username"
+          value=""
+          placeholder="username"
+          className="login_input"
+        />
+        <input type="text" label="username" value="" placeholder="username" />
+      </form>
+    </section>
+  );
 }
