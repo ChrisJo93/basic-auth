cfd7d0a7cb7c31a38eee7f7a8b0adc33c46dbaf7
Chris Johnson <Johnny.C.Alexander@gmail.com>
2021-10-07 00:27:16 -0500

    minor changes

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './login.css';
 
 export default function Login() {
   return (
     <section className="login_form_container">
-      <form className="login_form">
+      <form className="login_form_container">
         <input
           type="text"
           label="username"
@@ -11,7 +12,13 @@ export default function Login() {
           placeholder="username"
           className="login_input"
         />
-        <input type="text" label="username" value="" placeholder="username" />
+        <input
+          type="text"
+          label="password"
+          value=""
+          placeholder="password"
+          className="login_input"
+        />
       </form>
     </section>
   );
