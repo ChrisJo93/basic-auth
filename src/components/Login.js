add941fb5f69bb02e34336ccb9cd4bf4b1727369
Chris Johnson <Johnny.C.Alexander@gmail.com>
2021-10-07 17:35:26 -0500

    added material ui, added input change and submit handler

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,24 +1,44 @@
 import React from 'react';
+import { useState } from 'react';
 import './login.css';
 
 export default function Login() {
+  const [user, setUser] = useState('');
+  const [pass, setPass] = useState('');
+
+  const handleChange = (e, state) => {
+    state(e.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    console.log(user, pass);
+
+    setUser('');
+    setPass('');
+
+    event.preventDefault();
+  };
+
   return (
-    <section className="login_form_container">
-      <form className="login_form_container">
+    <section>
+      <form className="login_form_container" onSubmit={handleSubmit}>
         <input
           type="text"
           label="username"
-          value=""
+          value={user}
           placeholder="username"
           className="login_input"
+          onChange={(e) => handleChange(e, setUser)}
         />
         <input
           type="text"
           label="password"
-          value=""
+          value={pass}
           placeholder="password"
           className="login_input"
+          onChange={(e) => handleChange(e, setPass)}
         />
+        <button type="submit">Submit</button>
       </form>
     </section>
   );
