93ae1163fbcdf5459a72e51f9080c487e9a013df
Chris Johnson <Johnny.C.Alexander@gmail.com>
2021-10-08 17:40:57 -0500

    server added, pg added

---
+++ server/pool.js
@@ -0,0 +1,45 @@
+const pg = require('pg');
+const url = require('url');
+
+let config = {};
+
+if (process.env.DATABASE_URL) {
+  // Heroku gives a url, not a connection object
+  // https://github.com/brianc/node-pg-pool
+  const params = url.parse(process.env.DATABASE_URL);
+  const auth = params.auth.split(':');
+
+  config = {
+    user: auth[0],
+    password: auth[1],
+    host: params.hostname,
+    port: params.port,
+    database: params.pathname.split('/')[1],
+    ssl: { rejectUnauthorized: false },
+    max: 10,
+    idleTimeoutMillis: 30000,
+  };
+} else {
+  config = {
+    host: 'localhost',
+    port: 5432,
+    database: 'portfolio',
+    max: 10,
+    idleTimeoutMillis: 30000,
+  };
+}
+
+// this creates the pool that will be shared by all other modules
+const pool = new pg.Pool(config);
+
+pool.on('connect', () => {
+  console.log('pool connected');
+});
+// the pool with emit an error on behalf of any idle clients
+// it contains if a backend error or network partition happens
+pool.on('error', (err) => {
+  console.log('Unexpected error on idle client', err);
+  process.exit(-1);
+});
+
+module.exports = pool;
