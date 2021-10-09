260f323c041f22f03aa209a035845746ebe0686c
Chris Johnson <Johnny.C.Alexander@gmail.com>
2021-10-09 17:10:20 -0500

    added bcrypt salting middleware

--- server/route.js
+++ server/route.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const pool = require('./pool');
+const pool = require('./modules/pool');
 const router = express.Router();
+const encryptLib = require('./');
 
 router.get('/', (req, res) => {
   const queryCred = `SELECT * FROM "user";`;
@@ -12,6 +13,21 @@ router.get('/', (req, res) => {
     })
     .catch((error) => {
       console.log(`Error fetching user creds, ${error}`);
+      res.sendStatus(500);
+    });
+});
+
+router.post('/register', (req, res) => {
+  const username = req.body.username;
+  const password = encryptLib.encryptPassword(req.body.password);
+
+  const queryCred = `INSERT INTO "user" (username, password) VALUES ($1, $2) RETURNING id;`;
+
+  pool
+    .query(queryCred, [username, password])
+    .then(() => res.sendStatus(201))
+    .catch((error) => {
+      console.log(`User Registration error, ${error}`);
       res.sendStatus(500);
     });
 });
