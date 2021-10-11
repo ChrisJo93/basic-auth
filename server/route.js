4a1e9184be8206d55cb25b0a6d4c00a7911e53a5
Chris Johnson <Johnny.C.Alexander@gmail.com>
2021-10-10 19:54:04 -0500

    added encrpyption and edited route

--- server/route.js
+++ server/route.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const pool = require('./modules/pool');
 const router = express.Router();
-const encryptLib = require('./encryption');
+const encryptLib = require('./modules/encryption');
 
 router.get('/', (req, res) => {
   const queryCred = `SELECT * FROM "user";`;
@@ -20,7 +20,6 @@ router.get('/', (req, res) => {
 router.post('/register', (req, res) => {
   const username = req.body.username;
   const password = encryptLib.encryptPassword(req.body.password);
-
   const queryCred = `INSERT INTO "user" (username, password) VALUES ($1, $2) RETURNING id;`;
 
   pool
