e023d8ddccbcd73d6cc85c8014836bd5eddfbfe6
Chris Johnson <Johnny.C.Alexander@gmail.com>
2021-10-16 15:20:17 -0500

    added router to app.

--- src/App.js
+++ src/App.js
@@ -12,8 +12,20 @@ import {
 function App() {
   return (
     <main>
-      <Nav />
-      <Register />
+      <Router>
+        <div>
+          <Nav />
+
+          <Switch>
+            <Route path="/register">
+              <Register />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </main>
   );
 }
